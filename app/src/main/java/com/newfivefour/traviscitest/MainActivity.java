0925f34b61eaea0016970b795cf89f7619f99d41
newfivefour <newfivefour@newfivefour.com>
2016-08-19 11:42:46 +0200

    Integrated HockeyApp.

--- app/src/main/java/com/newfivefour/traviscitest/MainActivity.java
+++ app/src/main/java/com/newfivefour/traviscitest/MainActivity.java
@@ -3,11 +3,45 @@ package com.newfivefour.traviscitest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import net.hockeyapp.android.CrashManager;
+import net.hockeyapp.android.UpdateManager;
+
 public class MainActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    checkForUpdates();
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    checkForCrashes();
+  }
+
+  @Override
+  public void onPause() {
+    super.onPause();
+    unregisterManagers();
+  }
+
+  @Override
+  public void onDestroy() {
+    super.onDestroy();
+    unregisterManagers();
+  }
+
+  private void checkForCrashes() {
+    CrashManager.register(this);
+  }
+
+  private void checkForUpdates() {
+    UpdateManager.register(this);
+  }
+
+  private void unregisterManagers() {
+    UpdateManager.unregister();
   }
 }
