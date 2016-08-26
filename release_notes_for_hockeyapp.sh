55702ff07fc0e602218eaa211c9611a8beeb6638
newfivefour <newfivefour@newfivefour.com>
2016-08-27 00:11:31 +0200

    Now with correctly named environmental variable.

--- release_notes_for_hockeyapp.sh
+++ release_notes_for_hockeyapp.sh
@@ -1,5 +1,5 @@
 # Get the latest app uploads
-curl -H "X-HockeyAppToken: $MY_TOKEN" \
+curl -H "X-HockeyAppToken: $HOCKEYAPP_TOKEN" \
 "https://rink.hockeyapp.net/api/2/apps/e1e4c963ad144f23a8787ac79c3d1954/app_versions?page=1" | \
 # Put every property on a separate line
 sed 's/,/\n/g' | \
