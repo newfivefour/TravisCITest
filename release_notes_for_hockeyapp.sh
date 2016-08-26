09515382dc2984b2d15edf4e09e5c0ce0c1352cf
newfivefour <newfivefour@newfivefour.com>
2016-08-27 00:25:41 +0200

    Fixed the release notes script.

--- release_notes_for_hockeyapp.sh
+++ release_notes_for_hockeyapp.sh
@@ -1,3 +1,5 @@
+# We need an initial bullet point for our list of commit logs
+echo -n "* "
 # Get the latest app uploads
 curl -H "X-HockeyAppToken: $HOCKEYAPP_TOKEN" \
 "https://rink.hockeyapp.net/api/2/apps/e1e4c963ad144f23a8787ac79c3d1954/app_versions?page=1" | \
@@ -11,8 +13,6 @@ grep notes | \
 head -n 1 | \
 # Find the commit information at the bottom of the notes
 sed -n 's/.*(commit:\([^)]*\)).*/\1/p' | \
-# We need an initial bullet point for our list of commit logs
-echo -n "* "
 # Let's find all the logs since that commit
 xargs -I '{}' git log {}..HEAD --pretty=format:'%s' --no-merges | \
 # Turn this newlines into <br>s since we need to pass this all as one line
